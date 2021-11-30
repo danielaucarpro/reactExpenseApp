aea6e4c2ed104dd920bcb3c6e23876e197a4b2ad
danielaucarpro <danielaucarpro@gmail.com>
2021-11-29 16:22:06 -0800

    i think i made a mistake, My addPayment didt have the submit btn

--- src/components/AddPayment/AddPayment.js
+++ src/components/AddPayment/AddPayment.js
@@ -2,18 +2,19 @@ import React, {useState,  useContext} from 'react';
 import './AddPayment.css'; 
 import { Link } from "react-router-dom";
 import {GlobalContext} from '../ContextAPI/GlobalState'; 
+//import {styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import { Button, FormControl, Input, InputAdornment, InputLabel, MenuItem } from '@mui/material';
+//import Grid from'@mui/material/Grid';
+import { Button, FormControl, Input, InputAdornment, InputLabel } from '@mui/material';
 import LabelIcon from '@mui/icons-material/Label';
 import TodayIcon from '@mui/icons-material/Today';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import LoginIcon from '@mui/icons-material/Login';
-import Select from 'react-select'; 
 
 
 export const AddPayment = () => {
     //Hook. create state and a function to manipulate the state (setText and setAmount)
-    const [text, setText] = useState([]);
+    const [text, setText] = useState('');
     const [date, setDate] = useState('');
     const [amount, setAmount] = useState(0); 
 
@@ -23,7 +24,7 @@ export const AddPayment = () => {
     const onSubmit = e => {
         e.preventDefault(); 
 
-        const newTransaction = {
+        const newTRansaction = {
             //generate random number id 
             id : Math.floor(Math.random() * 100000000), 
             //passinf value into object
@@ -33,34 +34,34 @@ export const AddPayment = () => {
         }
 
         //call addTransaction from context and passing newTransaction
-        addTransaction(newTransaction); 
+        addTransaction(newTRansaction); 
     }
+
 
 
     return (
         <>
            <h3>Add a Bill</h3>
-            
-           <Box sx={{ display: 'flex', alignItems: 'flex-end', m:5}}>
 
+           <Box sx={{ display: 'flex', alignItems: 'flex-end', m:5}}>
+            
               <form onSubmit = {onSubmit}>
                 <FormControl variant="standard" sx={{m:5}}>
                     <InputLabel htmlFor="text">
-                    Name
+                    Name of the Purchansed Item
                     </InputLabel>
                      {/* use onchange to update the state and call function to give us whatever is being typed */}
                     <Input
-                       type = "text" 
-                       value ={text} onChange={(e) => setText(e.target.value)} 
-                       placeholder="Enter text.."
                        id="input-with-icon-purchaseditem"
                        startAdornment={
                            <InputAdornment position="start">
                                <LabelIcon />
                            </InputAdornment>
                        }
+                       type = "text" 
+                       value ={text} onChange={(e) => setText(e.target.value)} 
+                       placeholder="Enter text.."
                     />
-                    
                 </FormControl>
 
                 <br />
@@ -71,15 +72,15 @@ export const AddPayment = () => {
                     </InputLabel>
                      {/* use onchange to update the state and call function to give us whatever is being typed */}
                     <Input
-                       type = "date" 
-                       value ={date} onChange={(e) => setDate(e.target.value)} 
-                       placeholder="Enter Date..."
                        id="input-with-icon-calender"
                        startAdornment={
                            <InputAdornment position="start">
                                <TodayIcon />
                            </InputAdornment>
                        }
+                       input type = "date" 
+                       value ={date} onChange={(e) => setDate(e.target.value)} 
+                       placeholder="Enter Date..."
                     />
                 </FormControl>
 
@@ -91,22 +92,21 @@ export const AddPayment = () => {
                     </InputLabel>
                      {/* use onchange to update the state and call function to give us whatever is being typed */}
                     <Input
-                       type="number" 
-                       value={amount} onChange={(e) => setAmount(e.target.value)} 
-                       placeholder="$ Enter amount..."ç
                        id="input-with-icon-calender"
                        startAdornment={
                            <InputAdornment position="start">
                                <MonetizationOnIcon />
                            </InputAdornment>
                        }
+                       input type="number" 
+                       value={amount} onChange={(e) => setAmount(e.target.value)} 
+                       placeholder="$ Enter amount..."
                     />
                 </FormControl>
                 
                 <br/>
-                    <Button　type="submit" variant="contained" sx={{m:5}}>Add Bill</Button>
+                    <Button type='submit'　variant="contained" sx={{m:5}}>Add Bill</Button>
                     <Link to='/overview'><LoginIcon /></Link>
-                
               </form>
             </Box>
         </>
