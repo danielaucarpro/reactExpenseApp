eea62b8b7951d2c63c1f3dba38f49858f628bfac
danielaucarpro <danielaucarpro@gmail.com>
2021-12-13 13:29:49 -0800

    fixed sign in and sign up frontend, another some toher bugs

--- src/components/SignIn&SignUp/SignIn.js
+++ src/components/SignIn&SignUp/SignIn.js
@@ -1,82 +1,101 @@
-//importing react router
-import { Link } from "react-router-dom";
-//importing icons
-import LogoutIcon from '@mui/icons-material/Login';
-import {useState} from "react"; 
-import {Button, Input, FormControl, InputLabel, Typography} from "@mui/material";
+//importing react
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+//importing material ui
+import { Button, Input, FormControl, InputLabel, Typography, InputAdornment } from "@mui/material";
 import Box from '@mui/material/Box';
-import './SignIn.css'; 
-
+//css
+import './SignIn.css';
+//icons
+import LockIcon from '@mui/icons-material/Lock';
+import EmailIcon from '@mui/icons-material/Email';
 
 const SignIn = () => {
 
-    const [email,setEmail] = useState(''); 
-    const [password, setPassword] = useState(''); 
+    let navigate = useNavigate();
+    let [error, setError] = useState(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleFormSubmit = async (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
+        let result = await event.target;
         //Call the API
-        const response = await fetch ('http://localhost:5000/auth/login', {
-            method: "POST",  
-            headers: {
-                'Content-Type': "application/json",
-            }, 
-            body: JSON.stringify({
-                email, 
-                password
+        if (email === '' || password === '') {
+            alert('Unvalid email or password! Please try again.');
+        } else {
+            const response = await fetch('http://localhost:5000/auth/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
             })
-        })
 
-        const data = await response.json(); 
+            const data = await response.json();
 
-        console.log(data); 
-        if(data.token) {
-            localStorage.setItem(`access-token`, data.token); 
+            console.log(data);
+            if (data.token) {
+                localStorage.setItem(`access-token`, data.token);
+                navigate('/overview');
+            } else if (result.error) {
+                setError(result.error)
+            }
+            alert(data.message);
+            setEmail('');
+            setPassword('');
         }
-        alert(data.message); 
     }
 
     return (
         <>
-           <Box sx={{ display: 'flex', alignItems: 'flex-end', m:5}}>
-                <form onSubmit={handleFormSubmit}>
-                    <Typography variant="h6" gutterBottom component="div" sx={{m:5, px:5}}>
-                        Sign In
-                    </Typography>
-                    <FormControl variant="standard" sx={{m:5}}>
+            <Box className="signin-container" sx={{ display: 'flex', alignItems: 'flex-end', m: 5 }}>
+                <form onSubmit={handleFormSubmit} className="signin-form">
+                    <div className="sign-title">
+                        <Typography variant="h6" gutterBottom component="div" sx={{ m: 5, px: 5 }}>
+                            Sign In
+                        </Typography>
+                    </div>
+                    <FormControl className="sign-input" variant="standard" sx={{ m: 5 }}>
                         <InputLabel htmlFor="email">
                             Email:
                         </InputLabel>
-                        <Input 
-                           type="email"
-                           value={email}  
-                           onChange={(e) => setEmail(e.target.value)} 
-                           placeholder="Email" 
+                        <Input
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <EmailIcon />
+                                </InputAdornment>
+                            }
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
                         />
                     </FormControl>
-
-                    <br />
-
-                    <FormControl variant="standard" sx={{m:5}}>
+                    <FormControl variant="standard" sx={{ m: 5 }}>
                         <InputLabel htmlFor="password">
                             Password:
                         </InputLabel>
-                        <Input 
-                           type="password"
-                           value={password} 
-                           onChange={(e) => setPassword(e.target.value)} 
-                           placeholder="Password" 
+                        <Input
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <LockIcon />
+                                </InputAdornment>
+                            }
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
                         />
                     </FormControl>
 
-                    <br />
+                    <Button type="submit" value="Login" variant="contained" sx={{ m: 2, px: 8 }}>Sign In</Button>
 
-                    <Button　type="submit" value="Login" variant="contained" sx={{m:5, px:6}}>Sign In</Button>
-                    
-                    <br />
-
-                    <Typography variant="subtitle1" gutterBottom component="div" sx={{m:2}}>
-                        Not A Member yet? 
+                    <Typography className="signin-member" variant="subtitle1" gutterBottom component="div" sx={{ m: 2 }}>
+                        Not A Member yet?
                         <Link to='/signUp'> Sign Up </Link>
                     </Typography>
 
